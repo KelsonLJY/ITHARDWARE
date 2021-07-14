47d70a508f68aa96bed60b54a9f28adbd38ec22f
Amy <ampyaephyonaing@gmail.com>
2021-07-14 09:28:19 +0800

    User Authentication implementing with Passport JS

--- routes/user.js
+++ routes/user.js
@@ -25,31 +25,21 @@ router.post('/user/register',(req,res)=>{
         res.redirect('/')
     })
     .catch(err=>console.log(err))
-}).
-post('/user/login', (req, res, next) => {
-    const credential = req.body;
-}).get('/api/test' , (req, res,next) => {
-    let user = User_model.findOne({email : 'ampyaephyonaing@gmail.com'}).then(repo =>{
-        // console.log(repo)
-        let is_true= bcrypt.compareSync('asdassword', repo.password);
-        console.log(is_true)
-    })
-})
-.post("/api/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-        console.log(user)
-      if (err) {
-        return next(err);
-      }
+}).post("/api/login", (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        
+        if (!user) {
+            return res.status(400).send([user, "Cannot log in", info])
+        }
   
-      if (!user) {
-        return res.status(400).send([user, "Cannot log in", info]);
-      }
-  
-      req.login(user, err => {
-        res.send("Logged in");
-      });
-    })(req, res, next);
-  });
+        req.login(user, (err) => {
+            res.send("Logged in")
+        })
+    })(req, res, next)
+  })
+
 
 module.exports=router;
