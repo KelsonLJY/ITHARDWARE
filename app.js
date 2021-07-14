b37df9a3b8d104941cb6b504c1e2d48616934814
Amy <amy.pyae@smbe.fujielectric.com>
2021-07-14 20:46:44 +0800

    User Authentication

--- app.js
+++ app.js
@@ -1,54 +1,31 @@
-const express = require('express');
-const mongoose=require('mongoose');
-const dotenv = require('dotenv');
+const express = require('express')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
-const UserModel = require('./model/User')
-const bcrypt = require('bcrypt')
-// getting the local authentication type
-const LocalStrategy = require('passport-local').Strategy
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+require("./config/database")
+
+/**
+ * Cookie
+ */
 app.use(cookieSession({
   name: 'mysession',
   keys: ['vueauthrandomkey'],
   maxAge: 24 * 60 * 60 * 1000 // 24 hours 
 }))
-/**
- * Connect To the database
- */
-mongoose.connect("mongodb+srv://amydev:Amy123!*@cluster0.ogfnn.mongodb.net/it_hardware?retryWrites=true&w=majority",{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useCreateIndex : true
-});
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.use(passport.initialize())
 app.use(passport.session())
-
-
 app.use(require("./routes/index"))
 app.use(require("./routes/user"))
 
-app.post("/api/login", (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-      if (err) {
-          return next(err);
-      }
-      
-      if (!user) {
-          return res.status(400).send([user, "Cannot log in", info])
-      }
-
-      req.login(user, (err) => {
-          res.send("Logged in")
-      })
-  })(req, res, next)
-})
+require("./config/passport")
 
 
 const authMiddleware = (req, res, next) => {
@@ -58,33 +35,6 @@ const authMiddleware = (req, res, next) => {
       return next()
   }
 }
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-}, 
-async (username, password, done) => {
- 
-  let _password = password;
-  let user = await UserModel.findOne({email : username}).exec();
-  let is_true= bcrypt.compareSync(_password, user.password);
-
-    if(is_true){
-      done(null, user)
-
-    } else {
-        done(null, false, {message: 'Incorrect username or password'})
-    }
-}
-))
-
-passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
-
-passport.deserializeUser(async(id, done) => {
-  let user = await UserModel.findById(id).exec();
-  done(null, user)
-})
 
 app.listen(port, function () {
     console.log('Server started on port ' + port);
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,15 @@
 const router=require('express').Router()
-const User_model=require('../model/User')
+const User=require('../model/User')
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const isAuth = require('../middleware/auth').isAuth;
 
 router.post('/user/register',(req,res)=>{
     const user=req.body;
     const salt = bcrypt.genSaltSync(15);
     const hash = bcrypt.hashSync(user.password, salt);
     
-    const newUser=new User_model({
+    const newUser=new User({
         email:user.email,
         password:hash,
         full_name:user.full_name,
@@ -18,28 +19,39 @@ router.post('/user/register',(req,res)=>{
         dob:user.dob,
         hp : user.hp,
         phone : user.mobile
-
     })
     newUser.save().then(()=>{
-        console.log("done")
         res.redirect('/')
     })
     .catch(err=>console.log(err))
-}).post("/api/login", (req, res, next) => {
+})
+.get('/api/protected-route', isAuth, (req, res, next) => {
+    res.send('You made it to the route.' +  req.user.full_name);
+})
+.get('/api/logout', (req, res, next) => {
+    req.logout();
+    res.send('Logged out');
+})
+.post("/api/login", (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
         if (err) {
             return next(err);
         }
-        
         if (!user) {
-            return res.status(400).send([user, "Cannot log in", info])
+            return res.status(400).send({
+                message : 'Invalid email or password'
+            })
         }
-  
+        
         req.login(user, (err) => {
-            res.send("Logged in")
+            res.send({
+                email : user.email,
+                full_name : user.full_name
+            })
         })
     })(req, res, next)
-  })
+})
+
 
 
 module.exports=router;
